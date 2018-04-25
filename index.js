da0dfcbda95a954b08a69116ba164115151d267e
Noah Valley <noah@boeggsli.ch>
2018-04-25 15:24:52 +0200

    webserver works!

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express');
+var vhost = require('vhost')
+
+// vhost apps
+var frontend = require('../festival-jups/servers/frontend');
+var backend = require('../festival-jups/servers/backend');
+var api = require('../festival-jups/servers/api');
+
+//start server
+var server = express();
+
+// apply the vhost middleware
+server.use(vhost('festival-jups.ch', frontend));
+server.use(vhost('admin.festival-jups.ch', backend));
+server.use(vhost('api.festival-jups.ch', api));
+
+// start server
+server.listen(80);
